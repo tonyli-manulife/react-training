21c4d23863344e59e2d1cb276b74260d3656d545
tonyli <tony_z_li@manulife.com>
2022-07-05 10:33:37 +0800

    add the page template

---
+++ src/ManulifePage.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+
+export function ManulifePage(props) {
+  return (
+    <div className='app'>
+      <div className='navigation'>
+        <div className='logo'>
+          <img alt='logo' src={logo} />
+        </div>
+      </div>
+      <div className='main'>
+        <div className='header'>
+          Welcome to React Training
+        </div>
+        <div className='content'>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  )
+}
