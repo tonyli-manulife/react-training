563452a6622856931899fdc3fe0d6741e058a732
tonyli <tony_z_li@manulife.com>
2022-07-14 11:09:17 +0800

    change the files

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 
 import { ManulifePage } from './ManulifePage';
-import ComponentDemo from './course-2/react-component';
-import PropsDemo from './course-2/react-props';
-import StateDemo from './course-2/react-state';
-import LifeCycleDemo from './course-2/react-lifecycle';
-import ReactEvents from './course-2/react-events';
-import ReactList from './course-2/react-list';
-import ReactStyle from './course-2/react-style';
-import ReactForm from './course-2/react-form';
-
-import ReactExercise from './course-2/react-exercise';
+import { Course2 } from './course-2';
 
 import './App.css';
 
 function App() {
   return (
     < ManulifePage>
-      {/* <ComponentDemo /> */}
-      {/* <PropsDemo /> */}
-      {/* <StateDemo /> */}
-      {/* <LifeCycleDemo /> */}
-      {/* <ReactEvents /> */}
-      {/* <ReactStyle /> */}
-       {/* <ReactList /> */}
-       <ReactExercise />
-      {/* <ReactForm /> */}
+      <Course2 />
     </ManulifePage>
   );
 }
