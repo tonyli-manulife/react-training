2e30d72c52861a0e4f1c49d1e87e00e2e7bcbf5e
tonyli <tony_z_li@manulife.com>
2022-07-27 18:24:21 +0800

    remove the route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import { ManulifePage } from './ManulifePage';
 import { Course2 } from './course-2';
-import Routes from './route';
+// import Routes from './route';
 import './App.css';
 
 function App() {
   return (
     < ManulifePage>
-      {/*<Course2 />*/}
-      <Routes/>
+      <Course2 />
+      {/* <Routes/> */}
     </ManulifePage>
   );
 }
